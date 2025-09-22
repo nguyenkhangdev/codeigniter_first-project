070c7263960605ae2b7ff6ba544f62c01d48b896
khang.trannguyen <nguyenkhangdev@gmail.com>
2025-09-22 14:44:55 +0700

    feat: task api (no auth)

---
+++ application/controllers/api/Tasks.php
@@ -0,0 +1,94 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * @property CI_Input $input
+ * @property Task_model $Task_model
+ */
+class Tasks extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Task_model');
+        $this->load->helper('api');
+    }
+
+    //CI3 not support nany method http so we must use check method in controller method index
+    public function index($id = null)
+    {
+        try {
+            $method = $this->input->method(TRUE); // GET | POST | PUT | DELETE
+
+            switch ($method) {
+                case 'GET':
+                    if ($id === null) {
+                        // GET /api/tasks
+                        $tasks = $this->Task_model->read_tasks();
+                        return response($tasks, "Get tasks successfully");
+                    } else {
+                        // GET /api/tasks/:id
+                        $task = $this->Task_model->read_task($id);
+                        if ($task) {
+                            return response($task, "Get task successfully");
+                        } else {
+                            return response(null, "Task not found", "error", 404);
+                        }
+                    }
+                    break;
+
+                case 'POST':
+                    // POST /api/tasks
+                    $data = json_decode($this->input->raw_input_stream, true);
+                    if (!$data || !isset($data['title']) || !isset($data['description']) || !isset($data['status'])) {
+                        return response(null, 'Invalid input', 'error', 400);
+                    }
+                    //Testing version
+                    $data['user_id'] = '1';
+
+                    $createdTask = $this->Task_model->create_task($data);
+                    if ($createdTask) {
+                        return response($createdTask, "Create task successfully", "success", 201);
+                    } else {
+                        return response(null, "Create failed", "error", 400);
+                    }
+                    break;
+
+                case 'PUT':
+                    // PUT /api/tasks/:id
+                    if ($id === null) {
+                        return response(null, 'Missing task id', 'error', 400);
+                    }
+                    $data = json_decode($this->input->raw_input_stream, true);
+                    if (!$data) {
+                        return response(null, 'Invalid request', 'error', 400);
+                    }
+                    $updatedTask = $this->Task_model->update_task($id, $data);
+                    if ($updatedTask) {
+                        return response($updatedTask, "Update task successfully");
+                    } else {
+                        return response(null, "Update failed", "error", 400);
+                    }
+                    break;
+
+                case 'DELETE':
+                    // DELETE /api/tasks/:id
+                    if ($id === null) {
+                        return response(null, 'Missing task id', 'error', 400);
+                    }
+                    $deletedTask = $this->Task_model->delete_task($id);
+                    if ($deletedTask) {
+                        return response($deletedTask, "Delete task successfully");
+                    } else {
+                        return response(null, "Delete failed", "error", 400);
+                    }
+                    break;
+
+                default:
+                    return response(null, "Method not allowed", "error", 405);
+            }
+        } catch (Exception $e) {
+            return response(null, $e->getMessage(), "error", 500);
+        }
+    }
+}
---
+++ application/models/Task_model.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Task_model extends CI_Model
+{
+    public function read_task($id)
+    {
+        return  $this->db->get_where('tasks', array('id' => $id))->row_array();
+    }
+
+    public function read_tasks()
+    {
+        return $this->db->get('tasks')->result_array();
+    }
+
+    public function create_task($data)
+    {
+        return  $this->db->insert('tasks', $data);
+    }
+
+    public function update_task($id, $data)
+    {
+        $this->db->where('id', $id);
+        return  $this->db->update('tasks', $data);
+    }
+
+    public function delete_task($id)
+    {
+        return $this->db->delete('tasks',  array('id' => $id));
+    }
+}
